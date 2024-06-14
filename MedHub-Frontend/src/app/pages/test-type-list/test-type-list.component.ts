d493041316ab11dc8487454fe6b4e3585e09dfde
Alex <alexandru.jurju@tntcomputers.ro>
2024-06-14 11:18:57 +0300

    added popup for creating new testType
    added a confirm delete dialog for test type
    
    added button and popup for updating test type

--- MedHub-Frontend/src/app/pages/test-type-list/test-type-list.component.ts
+++ MedHub-Frontend/src/app/pages/test-type-list/test-type-list.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {TestTypeService} from "../../shared/services/swagger";
+import {AddTestTypeDto, TestTypeDto, TestTypeService} from "../../shared/services/swagger";
 
 @Component({
   selector: 'app-test-type-list',
@@ -9,6 +9,14 @@ import {TestTypeService} from "../../shared/services/swagger";
 export class TestTypeListComponent implements OnInit {
 
   dataSource: any;
+  createTestTypePopupVisible = false;
+  newTestTypeName: string = '';
+
+  deletePopupVisible: boolean = false;
+  testTypeIdToDelete: number = 0;
+
+  updateTestTypeInfo: TestTypeDto = {} as TestTypeDto;
+  updatePopupVisible: boolean = false;
 
   constructor(
     private testTypeService: TestTypeService
@@ -17,11 +25,70 @@ export class TestTypeListComponent implements OnInit {
 
   // todo: paging
   ngOnInit(): void {
+    this.getAllTestTypes();
+  }
+
+  private getAllTestTypes() {
     this.testTypeService.getAllTestTypes().subscribe({
       next: data => {
         this.dataSource = data;
       }
+    });
+  }
+
+  showNewTestTypePopup() {
+    this.newTestTypeName = ''; // Clear the input field
+    this.createTestTypePopupVisible = true;
+  }
+
+  saveTestType() {
+    const addTestTypeDto: AddTestTypeDto = {
+      name: this.newTestTypeName
+    }
+
+    this.testTypeService.createTestType(addTestTypeDto).subscribe({
+      next: result => {
+        this.createTestTypePopupVisible = false;
+        this.getAllTestTypes();
+      }
     })
   }
 
+  openDeletePopup(testTypeId: number) {
+    this.testTypeIdToDelete = testTypeId;
+    this.deletePopupVisible = true;
+  }
+
+  deleteTestType() {
+    this.testTypeService.deleteTestType(this.testTypeIdToDelete).subscribe({
+      next: result => {
+        this.getAllTestTypes();
+        this.deletePopupVisible = false;
+      },
+      error: err => {
+
+      }
+    })
+  }
+
+  openUpdatePopup(testType: TestTypeDto) {
+    this.updateTestTypeInfo = {...testType};
+    this.newTestTypeName = testType.name;
+    this.updatePopupVisible = true;
+  }
+
+  updateTestType() {
+    const updatedTestTypeDto: TestTypeDto = {
+      name: this.newTestTypeName,
+      id: this.updateTestTypeInfo.id
+    };
+
+    this.testTypeService.updateTestType(this.updateTestTypeInfo.id, updatedTestTypeDto).subscribe({
+      next: result => {
+        this.updatePopupVisible = false;
+        this.getAllTestTypes();
+      }
+    })
+
+  }
 }
